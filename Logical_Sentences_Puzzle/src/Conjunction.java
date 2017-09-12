5f130973f8d054ec32e046d859c1d920eef96da8
DavidWei95036593 <dw36593@pausd.us>
2017-09-12 14:52:24 -0700

    Update Conjunction.java

--- Logical_Sentences_Puzzle/src/Conjunction.java
+++ Logical_Sentences_Puzzle/src/Conjunction.java
@@ -1,11 +1,13 @@
 
 public class Conjunction extends LogicalSentence {
-
+	private LogicalSentence l5;
 //	public Conjunction(PropositionConstant a) {
 //		super(a);
 //	}
 
 	public Conjunction(LogicalSentence l3, Negation negation) {
+		
+		this.l5 = l3;
 	}
 
 }
